438541ea8de3c82045c35b3f7048beb89fa8819a
Kavindu Kariyawasam <72915403+KavinduKariyawasam@users.noreply.github.com>
2025-07-06 22:59:12 +0530

    Remove the pdf page visualizing step

--- src/utils.py
+++ src/utils.py
@@ -1,19 +1,3 @@
-# from jdeskew.estimator import get_angle
-# from jdeskew.utility import rotate
-# import cv2
-
-# image = cv2.imread(r'D:\Projects\expense\data\images\5.jpg')
-
-# angle = get_angle(image)
-
-# output_image = rotate(image, angle)
-
-# cv2.imshow("Deskewed Image", output_image)
-# cv2.waitKey(0)
-
-
-# docuwarp
-
 from PIL import Image
 from docuwarp.unwarp import Unwarp
 import cv2
@@ -28,12 +12,12 @@ def unwarp_image(image_path):
     return unwarped_image
 
 
-def get_first_page_image(pdf_path, zoom=2):
-    doc = fitz.open(pdf_path)
+def get_first_page_image(file_bytes, zoom=2):
+    doc = fitz.open(stream=file_bytes, filetype="pdf")
     page = doc.load_page(0)
     mat = fitz.Matrix(zoom, zoom)
     pix = page.get_pixmap(matrix=mat)
     img = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
-    cv2.imshow("PDF Page Image", np.array(img))
-    cv2.waitKey(0)
+    # cv2.imshow("PDF Page Image", np.array(img))
+    # cv2.waitKey(0)
     return cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
