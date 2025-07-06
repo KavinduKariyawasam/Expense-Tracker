86053024ce035eb09604d88bd444ad991d863e28
Kavindu Kariyawasam <72915403+KavinduKariyawasam@users.noreply.github.com>
2025-07-06 23:00:34 +0530

    Read the input from file bytes

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+from calendar import c
 from src.ocr import ocr_with_easyocr, ocr_with_paddleocr, preprocess_image, preprocess
 from src.analyze import parse_invoice, categorize_and_sum_items
 from src.utils import get_first_page_image
 import cv2
 import json
+import numpy as np
 
-def run_ocr(img_path=None):
+def run_ocr_only(img_path=None):
     if not img_path:
         img_path = 'data/images/13.jpg'
 
@@ -20,13 +22,32 @@ def run_ocr(img_path=None):
     processed_img = preprocess_image(img)
     easy_ocr_text = ocr_with_easyocr(processed_img)
     parsed = parse_invoice(easy_ocr_text)
-    # print("Parsed easy ocr Invoice Data:\n", json.dumps(parsed, indent=2))
-    categorized = categorize_and_sum_items(parsed.get("items", []))
-    # print("Categorized Totals:\n", json.dumps(categorized, indent=2))
-    return parsed, categorized
+    return parsed
+
+def run_ocr_only_bytes(file_bytes=None, extension=None):
+    if file_bytes:
+        if extension.lower() in ['png', 'jpg', 'jpeg']:
+            img_array = np.frombuffer(file_bytes, np.uint8)
+            img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+        elif extension.lower() == 'pdf':
+            img = get_first_page_image(file_bytes)
+        else:
+            return None
+    else:
+        return None
+
+    processed_img = preprocess_image(img)
+    easy_ocr_text = ocr_with_easyocr(processed_img)
+    parsed = parse_invoice(easy_ocr_text)
+    return parsed
+
+def categorize_totals(parsed_invoice):
+    categorized = categorize_and_sum_items(parsed_invoice)
+    return categorized
 
 if __name__ == "__main__":
     img_path = input("Enter the path to the image file (or press Enter to use default): ").strip().strip('"')
-    parse_invoice, categorized = run_ocr(img_path)
+    parse_invoice = run_ocr_only(img_path)
+    categorized = categorize_totals(parse_invoice)
     print("Parsed Invoice Data:\n", json.dumps(parse_invoice, indent=2))
     print("Categorized Totals:\n", json.dumps(categorized, indent=2))
