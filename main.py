8f4009f10eadf2cee8c584c334c94149383ddad0
Kavindu Kariyawasam <72915403+KavinduKariyawasam@users.noreply.github.com>
2025-07-15 20:45:17 +0530

    mistral ai ocr added

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 from calendar import c
-from src.ocr import ocr_with_easyocr, ocr_with_paddleocr, preprocess_image, preprocess
+from multiprocessing import process
+from src.ocr import ocr_with_easyocr, ocr_with_paddleocr, preprocess_image, preprocess, mistral_ocr
 from src.analyze import parse_invoice, categorize_and_sum_items
 from src.utils import get_first_page_image
 import cv2
 import json
 import numpy as np
+
+def mistral_ocr_only(img_path=None):
+    if not img_path:
+        img_path = 'data/images/13.jpg'
+
+    if img_path.lower().endswith(".pdf"):
+        img = get_first_page_image(img_path)
+    else:
+        img = cv2.imread(img_path)
+
+    if img is None:
+        print(f"Error: Could not load image from {img_path}")
+        exit(1)
+
+    processed_img = preprocess_image(img)
+    mistral_ocr_text = mistral_ocr(processed_img)
+    parsed = parse_invoice(mistral_ocr_text)
+    return parsed
 
 def run_ocr_only(img_path=None):
     if not img_path:
