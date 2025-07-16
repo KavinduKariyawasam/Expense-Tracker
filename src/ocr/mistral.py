69e558dfdc27db1062e3b6b120976401a89baf18
Kavindu Kariyawasam <72915403+KavinduKariyawasam@users.noreply.github.com>
2025-07-16 09:55:54 +0530

    logging added

--- src/ocr/mistral.py
+++ src/ocr/mistral.py
@@ -2,9 +2,19 @@ import os, base64, cv2, numpy as np, logging
 from io import BytesIO
 from mistralai import Mistral
 from dotenv import load_dotenv
-from src.ocr.preprocess import preprocess_image 
+import logging
+from src.utils import ColorFormatter
+import sys
+# from src.ocr.preprocess import preprocess_image 
 
 load_dotenv()
+
+LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO").upper()
+
+handler = logging.StreamHandler(sys.stdout)
+formatter = ColorFormatter('%(asctime)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logging.basicConfig(level=LOG_LEVEL, handlers=[handler])
 MISTRAL_API_KEY = os.getenv("MISTRAL_API_KEY")
 client  = Mistral(api_key=MISTRAL_API_KEY)
 
@@ -17,12 +27,13 @@ def jpeg_bytes_from_ndarray(img: np.ndarray, quality: int = 90) -> bytes:
 def base64_from_bytes(b: bytes) -> str:
     return base64.b64encode(b).decode("utf-8")
 
-def mistral_ocr(image_path):
-    raw_bgr = cv2.imread(image_path)
-    if raw_bgr is None:
-        raise FileNotFoundError(image_path)
+def mistral_ocr(preprocessed):
+    logging.info("Starting OCR with Mistral...")
+    # raw_bgr = cv2.imread(image_path)
+    # if raw_bgr is None:
+    #     raise FileNotFoundError(image_path)
 
-    preprocessed = preprocess_image(raw_bgr)
+    # preprocessed = preprocess_image(raw_bgr)
 
     if preprocessed.dtype != np.uint8:
         preprocessed = cv2.normalize(preprocessed, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
